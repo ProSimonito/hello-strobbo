eb0e12ee9c2dfea7278ca36a214e15ef4f3e9129
Simon <simon.vereecke@live.com>
2024-01-08 16:26:47 +0100

    Add static data for exercise

--- Strobbo/Strobbo.Hello/Program.cs
+++ Strobbo/Strobbo.Hello/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿static IEnumerable<string> Companies()
+{
+    yield return "Ibis Hotels";
+    yield return "Spar";
+    yield return "McDonalds";
+    yield return "Bavet";
+    yield return "KRC Genk";
+    yield return "Burger King";
+    yield return "Otomat";
+    yield return "Holiday Inn";
+    yield return "Van Der Valk";
+    yield return "Versuz";
+}
+
+const string StrobboHeadline= "AutomatiseerJePersoneelsplanningEnTijdregistratie";
+const string StrobboSummary = "Een_All_in_oplossing_met_payroll_integratie,_automatische_Dimona_aangifte_en_digitale_tikklok";
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Strobbo!");
