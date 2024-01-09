e617873e458a64724b843cc523928fd1e7637515
Simon <simon.vereecke@live.com>
2024-01-09 23:21:01 +0100

    add sushi service

---
+++ Strobbo/Strobbo.Hello/Sushi.cs
@@ -0,0 +1,10 @@
+﻿namespace Strobbo.Hello
+{
+    public class Sushi
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public bool SmellsFishy { get; set; }
+
+    }
+}
