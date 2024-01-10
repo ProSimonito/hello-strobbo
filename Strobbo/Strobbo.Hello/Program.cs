55b195b5a1169fdbf584cc8e4922d24b18198309
Simon <simon.vereecke@live.com>
2024-01-10 07:42:42 +0100

    rework Companies

--- Strobbo/Strobbo.Hello/Program.cs
+++ Strobbo/Strobbo.Hello/Program.cs
@@ -1,18 +1,18 @@
-﻿static IEnumerable<string> Companies()
-{
-    yield return "Ibis Hotels";
-    yield return "Spar";
-    yield return "McDonalds";
-    yield return "Bavet";
-    yield return "KRC Genk";
-    yield return "Burger King";
-    yield return "Otomat";
-    yield return "Holiday Inn";
-    yield return "Van Der Valk";
-    yield return "Versuz";
-}
+﻿string[] Companies =
+    [
+        "Ibis Hotels",
+        "Spar",
+        "McDonalds",
+        "Bavet",
+        "KRC Genk",
+        "Burger King",
+        "Otomat",
+        "Holiday Inn",
+        "Van Der Valk",
+        "Versuz"
+    ];
 
-const string StrobboHeadline= "AutomatiseerJePersoneelsplanningEnTijdregistratie";
+const string StrobboHeadline = "AutomatiseerJePersoneelsplanningEnTijdregistratie";
 const string StrobboSummary = "Een_All_in_oplossing_met_payroll_integratie,_automatische_Dimona_aangifte_en_digitale_tikklok";
 
 // See https://aka.ms/new-console-template for more information
